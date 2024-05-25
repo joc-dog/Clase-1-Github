52f4f04332d92ebc13467836a0155f621bbe7f0e
jose polanco <soporte1947@gmail.com>
2024-05-24 19:17:15 -0500

    Tarea 3

--- cinema/src/App.jsx
+++ cinema/src/App.jsx
@@ -1,58 +1,71 @@
 import './App.css'
+import Title from './components/Title'
+import Image from './components/Image'
+import Details from './components/Details'
+import CharacterCard from './components/CharacterCard'
+import ImageMovie from '../public/prueba1.webp'
+import ImageMovieBarbie from '../public/barbie.jpg'
+import ImageMovieImpecable from '../public/impecable.jpg'
 
 function App() {
 
   return (
     <>
-     <div className='box-header'>
+        <div className='box-header'>
             <img src='../public/logo.png' alt='Logo' />
             <img src='../public/hamburger-icon.png' alt='Hamburguesa' />
         </div>
 
-            <div className='banner'>
-                <div>
-                    <div className='banner-titulo'>El reino del planeta de los simios</div>
-                    <div className='banner-subtitulo'>Ambientada varias generaciones en el futuro tras el reinado de César, en la que los simios son la especie dominante que vive en armonía...</div>
-                </div>
-            </div>
-         
-        <div className='gallery-paris'>
+        <div className='banner'>
+        <div>
+            <div className='banner-titulo'>El reino del planeta de los simios</div>
+            <div className='banner-subtitulo'>Ambientada varias generaciones en el futuro tras el reinado de César...</div>
+        </div>    
+        </div>
+
+        <div class="gallery-paris">
             <div>
-                <h3>Estrenos</h3>
-                <h1>SD</h1>
-                <div className='gallery-paris-pics'>
-                    <div className='pic1'></div>
-                    <div className='pic2'></div>
-                    <div className='pic3'></div>
-                    <div className='pic4'></div>
+                <h1>TAREA</h1>
+                <h3>Componentes</h3>
+                <div className="gallery-paris-pics">
+                    <div class="pic">
+                        <Title title="Planeta de los Simios" />
+                        <Image url={ImageMovie} movieName="Planeta de los Simios" />
+                        <Details genre="Ficción" status="18 años" />
+                    </div>
+                    <div className="pic">
+                        <Title title="Planeta de los Simios" />
+                        <Image url={ImageMovie} movieName="Planeta de los Simios" />
+                        <Details genre="Ficción" status="18 años" />
+                    </div>
+                    <div className="pic">
+                        <Title title="Planeta de los Simios" />
+                        <Image url={ImageMovie} movieName="Planeta de los Simios" />
+                        <Details genre="Ficción" status="18 años" />
+                    </div>
+                    <div className="pic">
+                        <Title title="Planeta de los Simios" />
+                        <Image url={ImageMovie} movieName="Planeta de los Simios" />
+                        <Details genre="Ficción" status="18 años" />
+                    </div>
                 </div>
             </div>
         </div>
-        <hr />
-        <div className='gallery-berlin'>
+
+
+        <div class="gallery-paris">
             <div>
-                <h3>Mejoradas</h3>
-                <h1>HD</h1>
-                <div className='gallery-berlin-pics'>
-                    <div className='pic1'></div>
-                    <div className='pic2'></div>
-                    <div className='pic3'></div>
-                    <div className='pic4'></div>
+                <h1>RETO</h1>
+                <h3>CharacterCard</h3>
+                <div className="gallery-paris-pics">
+                        <CharacterCard title="Planeta de los Simios" url={ImageMovie} movieName="Planeta de los Simios" genre="Ficción" status="18 años" />
+                        <CharacterCard title="Barbie" url={ImageMovieBarbie} movieName="Barbie" genre="Comedia" status="12 años" />
+                        <CharacterCard title="Impecable" url={ImageMovieImpecable} movieName="Impecable" genre="Acción" status="12 años" />
                 </div>
             </div>
         </div>
-        <hr />
-        <div className='box-footer'>
-            <div className='social'>
-                <img src='../public/social_facebook.png' alt='facebook' />
-                <img src='../public/social_instagram.png' alt='Instagram' />
-                <img src='../public/social_youtube.png' alt='youtube' />
-                <img src='../public/social_twitter.png' alt='X' />
-            </div>
-            <div>
-                <h3>2024 CINEMA | COPYRIGHT</h3>
-            </div>
-        </div>
+
+
     </>
   )
 }
